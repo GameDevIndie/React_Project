d896379aa022cc270cf8301d97de722699dee2c3
ArunKumar Pullamsetty <pullamsettyaun@gmail.com>
2021-08-27 16:51:43 +0530

    1st commit

--- src/components/Forms/LoginForm.js
+++ src/components/Forms/LoginForm.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-//import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 //import Header from '../Layout/Header';
 //import Home from '../HomePage/Home';
@@ -14,11 +14,14 @@ const LoginForm = () => {
   const name = 'root';
   const pass = 'root';
 
+  let history = useHistory();
+
   const onSubmitHandler = (event) => {
     event.preventDefault();
     console.log('Hi');
     if (enteredUsername === name && enteredPassword === pass) {
-      return ;
+      console.log('Hi');
+      history.push('/courses');
     }
   };
 
@@ -26,24 +29,26 @@ const LoginForm = () => {
     <Card>
       <form onSubmit={onSubmitHandler} className={classes.input}>
         <label className={classes.login}>Log In</label>
-        <label
-          htmlFor='username'
-          className={classes.label}
-          value={enteredUsername}
-          onChange={(event) => setEnteredUsername(event.target.value)}
-        >
+        <label htmlFor='username' className={classes.label}>
           Username
         </label>
-        <input id='username' type='text' className={classes.input}></input>
-        <label
-          htmlFor='password'
-          className={classes.label}
-          value={enteredPassword}
-          onChange={(event) => setEnteredPassword(event.target.value)}
-        >
+        <input
+          id='username'
+          type='text'
+          className={classes.input}
+          value={enteredUsername}
+          onChange={(event) => setEnteredUsername(event.target.value)}
+        ></input>
+        <label htmlFor='password' className={classes.label}>
           Password
         </label>
-        <input id='password' type='password' className={classes.input}></input>
+        <input
+          id='password'
+          type='password'
+          className={classes.input}
+          value={enteredPassword}
+          onChange={(event) => setEnteredPassword(event.target.value)}
+        ></input>
         <Button type='submit'>Submit</Button>
       </form>
     </Card>
